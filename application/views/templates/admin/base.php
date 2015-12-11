168a2088a0e11d7e7b068ccbf8cc2d35606ec453
illiiiillllilii <vitalik7tv>
2015-12-11 17:31:11 +0300

    edited form page for main settings

--- application/views/templates/admin/base.php
+++ application/views/templates/admin/base.php
@@ -1,34 +1,57 @@
-<h3 class="p30">Настройки сайта</h3>
+<div class="page_form">
+
+<h2 class="form_name">Настройки сайта</h2>
 
 
 <form action="/admin/base" method="post">
 
 	<?= Form::hidden('csrf', Security::token()); ?>
 
-	<label>Title</label>
-	<input type="text" name="title" value="<?= $site_info->title ?>">
+	<h4>Title</h4>
+	<div class="input_text mb30">
+		<input type="text" name="title" value="<?= $site_info->title ?>" />
+	</div>
 
-	<label>Full name</label>
-	<input type="text" name="full_name" value="<?= $site_info->full_name ?>">
+	<h4>Full name</h4>
+	<div class="input_text mb30">
+		<input type="text" name="full_name" value="<?= $site_info->full_name ?>" />
+	</div>
 
-	<label>Description</label>
-	<input type="text" name="description" value="<?= $site_info->description ?>">
+	<h4>Description</h4>
+	<div class="input_text mb30">
+		<input type="text" name="description" value="<?= $site_info->description ?>" />
+	</div>
 
-	<label>Address</label>
-	<input type="text" name="address" value="<?= $site_info->address ?>">
+	<h4>Address</h4>
+	<div class="input_text mb30">
+		<input type="text" name="address" value="<?= $site_info->address ?>" />
+	</div>
 
-	<label>Phone</label>
-	<input type="text" name="phone" value="<?= $site_info->phone ?>">
+	<? /*<div class="w50 fl_l"> */ ?>
+		<h4>Phone</h4>
+		<div class="input_text mb30">
+			<input type="text" name="phone" value="<?= $site_info->phone ?>" />
+		</div>
+	<? /*</div>
+	<div class="w50 fl_l"> */ ?>
+		<h4>Fax</h4>
+		<div class="input_text mb30">
+			<input type="text" name="fax" value="<?= $site_info->fax ?>" />
+		</div>
+	<? /*</div> */ ?>
 
-	<label>Fax</label>
-	<input type="text" name="fax" value="<?= $site_info->fax ?>">
+	<h4>Email</h4>
+	<div class="input_text mb30">
+		<input type="text" name="email" value="<?= $site_info->email ?>" />
+	</div>
 
-	<label>Email</label>
-	<input type="text" name="email" value="<?= $site_info->email ?>">
+	<h4>Logo</h4>
+	<div class="input_text mb30">
+		<input type="text" name="logo" value="<?= $site_info->logo ?>" />
+	</div>
 
-	<label>Logo</label>
-	<input type="text" name="logo" value="<?= $site_info->logo ?>">
-	
-	<input class="mt20" type="submit" value="Опубликовать">
+	<input class="mt20" type="submit" value="Сохранить">
 
 </form>
+
+</div>
