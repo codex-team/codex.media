225366535c72296cc91d1cdaf5a8a4ee0d7fa129
Vladislav Tretyak <tretyak.1996@gmail.com>
2015-12-25 13:06:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ define('EXT', '.php');
  * When using a legacy application with PHP >= 5.3, it is recommended to disable
  * deprecated notices. Disable with: E_ALL & ~E_DEPRECATED
  */
-//error_reporting(E_ALL | E_STRICT);
-error_reporting(E_ALL & ~E_DEPRECATED);
+error_reporting(E_ALL | E_STRICT);
+//error_reporting(E_ALL & ~E_DEPRECATED);
 
 /**
  * End of standard configuration! Changing any of the code below should only be
