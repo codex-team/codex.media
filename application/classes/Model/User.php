84e9b3947b980fd7528c09e0b718e9d89bd0108f
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-01-21 21:09:50 +0300

    Revert "Stupid bug fixed"
    This reverts commit 1bbd574b332e3385c3c0779daec028b8850363cf.

--- application/classes/Model/User.php
+++ application/classes/Model/User.php
@@ -115,7 +115,7 @@ class Model_User extends Model_preDispatch
         return (int)$this->redis->get('user:'.$this->id.':online:timestamp');
     }
 
-
+    
     public function setAuthCookie($id)
     {
         $id = (int)$id;
