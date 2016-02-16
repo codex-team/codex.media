5cf2ca7dc07ee985f783b3212038705e3e0c60d1
neSpecc <specc.dev@gmail.com>
2016-02-16 21:53:21 +0300

    articles list rich-view design

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -8,12 +8,17 @@
 
     <? foreach ($pages as $page): ?>
 
-        <article class="post"> <?= $page->rich_view ?>
+        <article class="post <?= $page->rich_view ? 'rich_view' : '' ?>">
             <time><?= date_format(date_create($page->date), 'd F Y, G:i') ?></time>
-            <h3><a href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a></h3>
-            <div class="body">
-                <?= $page->content ?>
-            </div>
+            <h3>
+                <a href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a>
+            </h3>
+
+            <? if (!$page->rich_view): ?>
+                <div class="body">
+                    <?= $page->content ?>
+                </div>
+            <? endif ?>
             <div class="footer">
                 <a class="read_more" href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">Подробнее</a>
                 <a href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">Комментировать</a>
