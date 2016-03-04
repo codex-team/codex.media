c26f90cd348d7fe14e35a0913dfc2128125ecf05
neSpecc <specc.dev@gmail.com>
2016-03-04 16:32:26 +0300

    Article body parsing by <p> tag

--- application/classes/Controller/Parser.php
+++ application/classes/Controller/Parser.php
@@ -21,17 +21,22 @@ class Controller_Parser extends Controller_Base_preDispatch {
             $doc = new DOMDocument();
             libxml_use_internal_errors(true);
 
-            $encoding = mb_detect_encoding($page);
-            $doc->loadHTML(mb_convert_encoding($page, 'HTML-ENTITIES', $encoding));
+            $doc->recover = true;
+            $doc->strictErrorChecking = false;
+
+            $doc->loadHTML($page);
+
+            // $encoding = mb_detect_encoding($page);
+            // $doc->loadHTML(mb_convert_encoding($page, 'HTML-ENTITIES', $encoding));
 
             libxml_clear_errors();
-            
+
             $return = [
                 'title'     => self::getTitle($doc),
-                'article'   => self::getBody($doc)
+                'article'   => self::getArticleText($doc)
                 ];
 
-            #var_dump($return);    
+            echo Debug::vars( $return );
         }
     }
 
@@ -43,7 +48,7 @@ class Controller_Parser extends Controller_Base_preDispatch {
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0 " );
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
@@ -88,7 +93,7 @@ class Controller_Parser extends Controller_Base_preDispatch {
     /*
     * Получаем тело страницы
     */
-    public function getBody($doc)
+    public function _getBody($doc)
     {
         $pageArticle    = '';
         $article        = $doc->getElementsByTagName('article');
@@ -102,4 +107,108 @@ class Controller_Parser extends Controller_Base_preDispatch {
         return $pageArticle;
     }
 
-}    
+    /*
+    * Parses html page and extracts article text
+    * Looks for node with many paragraph-tags
+    */
+    public function getArticleText($doc)
+    {
+        /**
+        * Working with all paragraphs on page
+        */
+        $paragraphs = $doc->getElementsByTagName('p');
+
+        /**
+        * Collect information about each <p> parent element.
+        *
+        * Save it in parents array:
+        *   array = (
+        *       'DIV@article_content' => array(
+        *           node   => DOMElement    // cursor to node
+        *           childs => 12            // paragraphs count
+        *       ),
+        *       ...
+        *   )
+        */
+        $parents = array();
+
+        for ($i = 0; $i < $paragraphs->length; $i++) {
+
+            $parentNode = $paragraphs->item($i)->parentNode;
+
+            /** Compose node text-identifier looks like 'TAGNAME@classname' */
+            $parentNodeIdentifier = self::getNodeIdentifier($parentNode);
+
+            if ( !isset($parents[$parentNodeIdentifier]) ){
+
+                $parents[$parentNodeIdentifier] = array(
+                    'node'   => $parentNode,
+                    'childs' => 1
+                );
+
+            } else {
+
+                $parents[$parentNodeIdentifier]['childs']++;
+
+            }
+
+        }
+
+        /**
+        * Now, get parent-node with maximum paragraphs count
+        * It might be an article we look for.
+        */
+        $maximumParagraphsCount    = 0;
+        $nodeWithMaximumParagraphs = null;
+
+        foreach ($parents as $item) {
+            if ($item['childs'] > $maximumParagraphsCount) {
+                $maximumParagraphsCount    = $item['childs'];
+                $nodeWithMaximumParagraphs = $item['node'];
+            }
+        }
+
+        /**
+        * Extract HTML-content from article node
+        */
+        $articleContent = self::DOMinnerHTML($nodeWithMaximumParagraphs);
+
+        return $articleContent;
+    }
+
+    /**
+    * Compose node text-identifier
+    * @todo add another attributes. Many elements can be without classname
+    * @return string tagname@classname. Example: 'DIV@article_content'
+    */
+    private static function getNodeIdentifier(DOMNode $node)
+    {
+        $tagName   = $node->nodeName;
+        $className = '';
+
+        if ( $classAttr = $node->attributes->getNamedItem('class') ){
+            $className = $classAttr->nodeValue;
+        }
+
+        return $tagName . '@' . $className;
+    }
+
+    /**
+    * Returns DOMNode inner html content
+    */
+    private static function DOMinnerHTML(DOMNode $element)
+    {
+
+        $innerHTML = '';
+        $children  = $element->childNodes;
+
+        foreach ($children as $child)
+        {
+            $innerHTML .= $element->ownerDocument->saveHTML($child);
+        }
+
+        return $innerHTML;
+
+    }
+
+}
