19510db4c9b556e335a31d1260b21f64df99e3d3
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-04-08 20:53:14 +0300

    Removed excess spaces, changed author field to uder_id

--- application/classes/Model/Comment.php
+++ application/classes/Model/Comment.php
@@ -10,15 +10,15 @@ Class Model_Comment extends Model_preDispatch
     public $parent_comment;
     public $dt_create;
     public $is_removed;
-    
+
     public function __construct()
     {
     }
 
-    /** 
+    /**
      * Возвращает комментарий с указанным id из БД.
      * Иначе возвращает пустой комментарий с id = 0.
-     */	
+     */
     public static function get($id = 0)
     {
         $comment_row = Dao_Comments::select()
@@ -26,33 +26,33 @@ Class Model_Comment extends Model_preDispatch
             ->limit(1)
             ->cached(Date::MINUTE * 5, 'comment:' . $id)
             ->execute();
-        
+
         $model = new Model_Comment();
-        
+
         return $model->fillByRow($comment_row);
     }
 
-    /** 
+    /**
      * Добавляет комментарий в БД.
-     */	
+     */
     public function insert()
     {
         $idAndRowAffected = Dao_Comments::insert()
-            ->set('author',    $this->author['id'])
+            ->set('user_id',   $this->author['id'])
             ->set('text',      $this->text)
             ->set('page_id',   $this->page_id)
             ->set('root_id',   $this->root_id)
             ->set('parent_id', $this->parent_comment['id'])
             ->clearcache('page:' . $this->page_id)
             ->execute();
-        
+
         if ($this->root_id == 0) {
             Dao_Comments::update()
                 ->where('id', '=', $idAndRowAffected)
                 ->set('root_id', $idAndRowAffected)
-                ->execute(); 
+                ->execute();
         }
-        
+
         return $idAndRowAffected;
     }
 
@@ -62,38 +62,38 @@ Class Model_Comment extends Model_preDispatch
     private function fillByRow($comment_row)
     {
         if (!empty($comment_row['id'])) {
-            
+
             $this->id         = $comment_row['id'];
-            $this->author     = new Model_User($comment_row['author']);
+            $this->author     = new Model_User($comment_row['user_id']);
             $this->text       = $comment_row['text'];
             $this->page_id    = $comment_row['page_id'];
             $this->root_id    = $comment_row['root_id'];
             $this->dt_create  = $comment_row['dt_create'];
             $this->is_removed = $comment_row['is_removed'];
         }
-        
+
         return $this;
     }
-    
+
     private static function getParentFromCommentsArray($comment_rows, $comment_row)
     {
         foreach ($comment_rows as $parent_row) {
             if ($parent_row['id'] == $comment_row['parent_id']) {
                 $parent = array(
                     "id"         => $parent_row['id'],
-                    "author"     => new Model_User($parent_row['author']),
+                    "author"     => new Model_User($parent_row['user_id']),
                     "text"       => $parent_row['text'],
                     "root_id"    => $parent_row['root_id'],
                     "dt_create"  => $parent_row['dt_create'],
                     "is_removed" => $parent_row['is_removed']
                 );
                 break;
-            }                        
+            }
         }
-        
+
         return $parent;
     }
-    
+
     public static function getCommentsByPageId($page_id)
     {
         $comments = array();
@@ -104,23 +104,22 @@ Class Model_Comment extends Model_preDispatch
             ->order_by('root_id', 'ASC')
             ->cached(Date::MINUTE * 5, 'page:' . $page_id)
             ->execute();
-        
+
         if ($comment_rows) {
             foreach ($comment_rows as $comment_row) {
                 $parent = array();
-                
+
                 $parent = self::getParentFromCommentsArray($comment_rows, $comment_row);
-                    
+
                 $comment = new Model_Comment();
 
                 $comment->fillByRow($comment_row);
-                
+
                 $comment->parent_comment = $parent;
 
                 array_push($comments, $comment);
             }
         }
-
         return $comments;
     }
 
@@ -133,17 +132,17 @@ Class Model_Comment extends Model_preDispatch
             ->where('id', '=', $this->id)
             ->set('is_removed', 1)
             ->clearcache('page:' . $this->page_id)
-            ->execute(); 
-            
+            ->execute();
+
         Dao_Comments::update()
             ->where('root_id', '=', $this->id)
             ->set('is_removed', 1)
-            ->execute(); 
-        
+            ->execute();
+
         Dao_Comments::update()
             ->where('parent_id', '=', $this->id)
             ->set('is_removed', 1)
-            ->execute(); 
+            ->execute();
     }
 
 
