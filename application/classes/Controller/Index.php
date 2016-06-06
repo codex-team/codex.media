cf5362515d886d4206da4fada591ace4d3f5a312
Taly <guryn@yandex.ru>
2016-06-06 08:55:31 +0300

    ajax response from index.php

--- application/classes/Controller/Index.php
+++ application/classes/Controller/Index.php
@@ -3,14 +3,57 @@
 class Controller_Index extends Controller_Base_preDispatch
 {
 
+    const NEWS_LIMIT_PER_PAGE = 4;
+
     public function action_index()
     {
-        $this->view['pages'] = Model_Page::getPages(Model_Page::TYPE_SITE_NEWS, 10, 0, 0, true);
-        $this->template->content = View::factory('templates/index', $this->view);
+        $page_number = $this->request->param('page_number');
+        if (!$page_number)
+        {
+            $page_number = 1;
+        }
+
+        $offset = ($page_number - 1) * self::NEWS_LIMIT_PER_PAGE;
+
+        $pages = Model_Page::getPages(
+                                    Model_Page::TYPE_SITE_NEWS, 
+                                    self::NEWS_LIMIT_PER_PAGE + 1, 
+                                    $offset);  
+
+        $next_page = false;
+        if (count($pages) > self::NEWS_LIMIT_PER_PAGE)
+        {
+            $next_page = true;
+            unset($pages[self::NEWS_LIMIT_PER_PAGE]);
+
+        }
+
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'))
+        {
+            $response = array();
+            $response['success']    = 1;
+            $response['next_page']  = $next_page;   
+            $response['pages']      = Model_Page::modelToArray(null, $pages);
+
+            $this->auto_render = false;
+            $this->response->headers('Content-Type', 'application/json; charset=utf-8');
+            $this->response->body( json_encode($response) );
+
+        } else {
+
+            $this->view['pages']        = $pages;
+            $this->view['next_page']    = $next_page;
+            $this->view['page_number']  = $page_number;
+
+            $this->template->content = View::factory('templates/index', $this->view);
+
+        }
     }
+
     public function action_contacts()
     {
         $this->template->title = 'Контакты';
         $this->template->content = View::factory('templates/contacts', $this->view);
     }
+    
 }
