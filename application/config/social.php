c45d5484f4f237abe9616b548593b188c3d0658d
loopdigga98 <tretyak.1996@gmail.com>
2016-06-12 23:31:08 +0300

    сделал пустые конфиг файлы social.php, database.php

---
+++ application/config/social.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+    'vk' => array(
+        'type'          => 1,
+        'client_id'     => '',
+        'client_secret' => '',
+        'redirect_uri'  => 'http'.((Arr::get($_SERVER, 'HTTPS')) ? 's' : '').'://'.Arr::get($_SERVER, 'SERVER_NAME').'/auth/vk',
+        'scopes'        => '',
+    ),
+    'facebook' => array(
+        'type'         => 2,
+        'client_id'    => '',
+        'client_secret'=> '',
+        'response_type'=> '',
+        'scope'        => '',
+        'redirect_uri' =>  'http'. ((Arr::get($_SERVER, 'HTTPS')) ? 's' : '') .'://'.Arr::get($_SERVER, 'SERVER_NAME').'/auth/fb',
+    ),
+    'twitter' => array(
+        'type'           => 3,
+        'consumer_key'   => '',
+        'consumer_secret'=>'',
+        'redirect_uri' =>  'http'. ((Arr::get($_SERVER, 'HTTPS')) ? 's' : '') .'://'.Arr::get($_SERVER, 'SERVER_NAME').'/auth/tw',
+    ),
+);
