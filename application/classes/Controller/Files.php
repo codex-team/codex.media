81aee519f729294f24ee0082f505a3e17407cbe8
Taly <guryn@yandex.ru>
2016-07-30 13:10:09 +0300

    action: return file to user

--- application/classes/Controller/Files.php
+++ application/classes/Controller/Files.php
@@ -6,15 +6,9 @@ class Controller_Files extends Controller_Base_preDispatch
     {
         $filename = $this->request->param('filename');
 
-        // $file = file_get_contents('upload/page_files/' . $filename . );
-
-        // $this->response->headers('Content-disposition', 'attachment; filename=' . $filename . '.py');
-        // $this->auto_render = false;
-        // $this->response->body($file);
-
         $file = new Model_File(null, $filename);
-        //$file->update(array('extension' => 't'));
-        $this->response->body(json_encode($file));
-        $this->auto_render = false;
+
+        $file->returnFileToUser();
+
     }
 }
