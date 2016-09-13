ab089ff6b43d9109bd7fc5a3fa8c5c59aef0ed54
Peter Savchenko <specc.dev@gmail.com>
2016-09-13 16:47:00 +0300

    New font, Design tasks closes
    Closes
    #82
    #79
    #78
    #76

--- application/views/templates/admin/layout.php
+++ application/views/templates/admin/layout.php
@@ -1,27 +1,26 @@
-<h1 class="site_category_title">
-	<a href="/admin"><?= $title ?></a>	
-</h1>
-<div class="recommend_block">
-    Быстрые действия:
-    <a class="button" href="/admin/news/new">Добавить новость</a>
-	<a class="button" href="/admin/pages/new">Создать страницу</a>
-	<a class="button" href="/admin/files/new">Загрузить файл</a>
+<div class="w_island w_island_centercol">
+	<h1 class="page_title">
+		<?= $title ?>
+	</h1>
+	<div class="action-line action-line__onpage">
+	    <a class="button" href="/admin/news/new">Добавить новость</a>
+		<a class="button" href="/admin/pages/new">Создать страницу</a>
+		<a class="button" href="/admin/files/new">Загрузить файл</a>
+	</div>
+	<? if ($form_saved): ?>
+		<div class="form_saved">Изменения сохранены</div>
+	<? endif ?>
+	<? if (isset($error) && $error): ?>
+		<div class="form_error"><?= $error ?></div>
+	<? endif ?>
+	<? if ( in_array($category, Controller_Admin::$categories) ): ?>
+
+		<? if ( isset($pageId) && $pageId ) $category = 'page'; ?>
+		<? if ( $category == 'news' ) $category = 'pages'; ?>
+		<? include(APPPATH . '/views/templates/admin/' . $category . '.php'); ?>
+
+	<? endif; ?>
 </div>
-<? if ($form_saved): ?>
-	<div class="form_saved">Изменения сохранены</div>
-<? endif ?>
-<? if (isset($error) && $error): ?>
-	<div class="form_error"><?= $error ?></div>
-<? endif ?>
-<? if ( in_array($category, Controller_Admin::$categories) ): ?>
-	
-	<? if ( isset($pageId) && $pageId ) $category = 'page'; ?>
-	<? if ( $category == 'news' ) $category = 'pages'; ?>
-	<? include(APPPATH . '/views/templates/admin/' . $category . '.php'); ?>
-
-<? endif; ?>
-
-
 
 
 
