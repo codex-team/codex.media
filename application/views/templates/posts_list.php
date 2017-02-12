1f51280cc4d5bbf43aa0f210ab43d46cc0628830
Taly Guryn <vitalik7tv@yandex.ru>
2017-02-12 03:51:02 +0300

    Add empty_motivatior if no posts

--- application/views/templates/posts_list.php
+++ application/views/templates/posts_list.php
@@ -1,31 +1,44 @@
-<? foreach ($pages as $page): ?>
+<? if ($pages): ?>
 
-    <article class="island post-list-item <?= $page->rich_view ? 'post-list-item--big' : '' ?> <?= $page->dt_pin ? 'post-list-item_pinned' : '' ?>">
+    <? foreach ($pages as $page): ?>
 
-        <div class="post-list-item__header">
-            <time class="post-list-item__date">
-                <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
-                    <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
+        <article class="island island--margined post-list-item <?= $page->rich_view ? 'post-list-item--big' : '' ?> <?= $page->dt_pin ? 'post-list-item_pinned' : '' ?>">
+
+            <div class="post-list-item__header">
+                <time class="post-list-item__date">
+                    <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
+                        <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
+                    </a>
+                </time>
+                <a class="post-list-item__author" href="/user/<?= $page->author->id ?>">
+                    <img src="<?= $page->author->photo ?>" alt="<?= $page->author->name ?>"><?= $page->author->name ?>
                 </a>
-            </time>
-            <a class="post-list-item__author" href="/user/<?= $page->author->id ?>">
-                <img src="<?= $page->author->photo ?>" alt="<?= $page->author->name ?>"><?= $page->author->name ?>
-            </a>
-        </div>
-
-        <a class="post-list-item__title" href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a>
-
-        <? if (!$page->rich_view): ?>
-            <div class="post-list-item__body">
-                <?= $page->description ?>
             </div>
-        <? endif ?>
-        <div class="post-list-item__footer">
-            <a class="post-list-item__comments" href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">
-                <? include(DOCROOT . "public/app/svg/comment.svg") ?>
-                Комментировать
-            </a>
-        </div>
-    </article>
 
-<? endforeach; ?>
+            <a class="post-list-item__title" href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a>
+
+            <? if (!$page->rich_view): ?>
+                <div class="post-list-item__body">
+                    <?= $page->description ?>
+                </div>
+            <? endif ?>
+            <div class="post-list-item__footer">
+                <a class="post-list-item__comments" href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">
+                    <? include(DOCROOT . "public/app/svg/comment.svg") ?>
+                    Комментировать
+                </a>
+            </div>
+        </article>
+
+    <? endforeach; ?>
+
+<? else: ?>
+
+    <div class="island island--padded island--margined island--centered">
+        <div class="empty_motivatior">
+            <!-- <i class="icon_noarticles"></i><br/> -->
+            Постов нет
+        </div>
+    </div>
+
+<? endif ?>
