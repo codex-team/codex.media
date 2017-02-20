bfe4cf97fbd836363f61ef931462717bb47a08d4
Peter Savchenko <specc.dev@gmail.com>
2017-02-21 00:58:22 +0300

    writing actions bg imroved

--- application/views/templates/pages/form.php
+++ application/views/templates/pages/form.php
@@ -48,37 +48,39 @@
 
     </div>
 
-    <div class="actions clear">
+    <div class="writing__actions clear">
+        <div class="writing__actions-content">
 
-        <span class="button master fl_r" onclick="codex.transport.submitAtlasForm()">Отправить</span>
+            <span class="button master fl_r" onclick="codex.transport.submitAtlasForm()">Отправить</span>
 
-        <? if (!empty($hideEditorToolbar) && $hideEditorToolbar): ?>
-            <span class="button fl_r" onclick="codex.transport.openEditorFullscrean()">На весь экран</span>
-        <? endif ?>
+            <? if (!empty($hideEditorToolbar) && $hideEditorToolbar): ?>
+                <span class="button fl_r" onclick="codex.transport.openEditorFullscrean()">На весь экран</span>
+            <? endif ?>
 
-        <? /**
-        <? if ($user->isAdmin && $page->type == Model_Page::TYPE_SITE_NEWS): ?>
+            <? /**
+            <? if ($user->isAdmin && $page->type == Model_Page::TYPE_SITE_NEWS): ?>
 
-            <div class="toggler fl_r js-custom-checkbox <?= $page->dt_pin ? 'checked' : '' ?>" data-title="Закрепить новость">
-                <input type="checkbox" name="dt_pin" value="<?= $page->dt_pin ? $page->dt_pin : date('Y-m-d H:i:s') ?>" <?= isset($page->dt_pin) ? 'checked="checked"' : '' ?>/>
-                <i class="icon-pin"></i>
-            </div>
+                <div class="toggler fl_r js-custom-checkbox <?= $page->dt_pin ? 'checked' : '' ?>" data-title="Закрепить новость">
+                    <input type="checkbox" name="dt_pin" value="<?= $page->dt_pin ? $page->dt_pin : date('Y-m-d H:i:s') ?>" <?= isset($page->dt_pin) ? 'checked="checked"' : '' ?>/>
+                    <i class="icon-pin"></i>
+                </div>
 
-            <div class="toggler fl_r js-custom-checkbox <?= $page->rich_view ? 'checked' : '' ?>" data-title="Важная новость">
-                <input type="checkbox" name="rich_view" value="1" <?= isset($page->rich_view) && $page->rich_view == 1 ? 'checked="checked"' : Arr::get($_POST, 'rich_view' , '') ?>/>
-                <i class="icon-megaphone"></i>
-            </div>
+                <div class="toggler fl_r js-custom-checkbox <?= $page->rich_view ? 'checked' : '' ?>" data-title="Важная новость">
+                    <input type="checkbox" name="rich_view" value="1" <?= isset($page->rich_view) && $page->rich_view == 1 ? 'checked="checked"' : Arr::get($_POST, 'rich_view' , '') ?>/>
+                    <i class="icon-megaphone"></i>
+                </div>
 
-            <div class="hidden toggler fl_r js-custom-checkbox <?= $page->is_menu_item ? 'checked' : '' ?>" data-title="Пункт меню">
-                <input type="checkbox" name="is_menu_item" value="1" <?= isset($page->is_menu_item) && $page->is_menu_item == 1 ? 'checked="checked"' : Arr::get($_POST, 'is_menu_item' , '') ?>/>
-                <i class="icon-star"></i>
-            </div>
+                <div class="hidden toggler fl_r js-custom-checkbox <?= $page->is_menu_item ? 'checked' : '' ?>" data-title="Пункт меню">
+                    <input type="checkbox" name="is_menu_item" value="1" <?= isset($page->is_menu_item) && $page->is_menu_item == 1 ? 'checked="checked"' : Arr::get($_POST, 'is_menu_item' , '') ?>/>
+                    <i class="icon-star"></i>
+                </div>
 
-        <? endif ?>
-        */ ?>
+            <? endif ?>
+            */ ?>
 
-        <span class="attach" onclick="codex.transport.selectFile(event, '<?= Model_File::PAGE_FILE ?>')"><i class="icon-attach"></i></span>
-        <span class="attach" onclick="codex.transport.selectFile(event, '<?= Model_File::PAGE_IMAGE ?>')"><i class="icon-picture"></i></span>
+            <span class="attach" onclick="codex.transport.selectFile(event, '<?= Model_File::PAGE_FILE ?>')"><i class="icon-attach"></i></span>
+            <span class="attach" onclick="codex.transport.selectFile(event, '<?= Model_File::PAGE_IMAGE ?>')"><i class="icon-picture"></i></span>
+        </div>
 
     </div>
 
