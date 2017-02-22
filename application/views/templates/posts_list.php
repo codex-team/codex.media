9e3aecc2d2dd4782316328ee83768736e2f8089b
khaydarov <murod.haydarov@inbox.ru>
2017-02-22 20:37:31 +0300

    revert changes

--- application/views/templates/posts_list.php
+++ application/views/templates/posts_list.php
@@ -2,40 +2,37 @@
 
     <? foreach ($pages as $index => $page): ?>
 
-        <? if ($page->author): ?>
+        <? /**
+            * if the elem is the first
+            * $page === reset($pages)
+            */ ?>
+        <article class="island <?= $index != 0 ? 'island--margined' : '' ?> post-list-item <?= $page->rich_view ? 'post-list-item--big' : '' ?> <?= $page->dt_pin ? 'post-list-item_pinned' : '' ?>">
 
-            <? /**
-                * if the elem is the first
-                * $page === reset($pages)
-                */ ?>
-            <article class="island <?= $index != 0 ? 'island--margined' : '' ?> post-list-item <?= $page->rich_view ? 'post-list-item--big' : '' ?> <?= $page->dt_pin ? 'post-list-item_pinned' : '' ?>">
-
-                <div class="post-list-item__header">
-                    <time class="post-list-item__date">
-                        <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
-                            <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
-                        </a>
-                    </time>
-                    <a class="post-list-item__author" href="/user/<?= $page->author->id ?>">
-                        <img src="<?= $page->author->photo ?>" alt="<?= $page->author->name ?>"><?= $page->author->name ?>
+            <div class="post-list-item__header">
+                <time class="post-list-item__date">
+                    <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
+                        <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
                     </a>
-                </div>
+                </time>
+                <a class="post-list-item__author" href="/user/<?= $page->author->id ?>">
+                    <img src="<?= $page->author->photo ?>" alt="<?= $page->author->name ?>"><?= $page->author->name ?>
+                </a>
+            </div>
 
-                <a class="post-list-item__title" href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a>
+            <a class="post-list-item__title" href="/p/<?= $page->id ?>/<?= $page->uri ?>"><?= $page->title ?></a>
 
-                <? if (!$page->rich_view): ?>
-                    <div class="post-list-item__body">
-                        <?= $page->description ?>
-                    </div>
-                <? endif ?>
-                <div class="post-list-item__footer">
-                    <a class="post-list-item__comments" href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">
-                        <? include(DOCROOT . "public/app/svg/comment.svg") ?>
-                        Комментировать
-                    </a>
+            <? if (!$page->rich_view): ?>
+                <div class="post-list-item__body">
+                    <?= $page->description ?>
                 </div>
-            </article>
-        <? endif; ?>
+            <? endif ?>
+            <div class="post-list-item__footer">
+                <a class="post-list-item__comments" href="/p/<?= $page->id ?>/<?= $page->uri ?>" rel="nofollow">
+                    <? include(DOCROOT . "public/app/svg/comment.svg") ?>
+                    Комментировать
+                </a>
+            </div>
+        </article>
 
     <? endforeach; ?>
 
