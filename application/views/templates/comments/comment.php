a94fde55da9c9c80d159c95ce8ac3e3ea2a889e4
Taly Guryn <vitalik7tv@yandex.ru>
2017-02-28 06:30:32 +0300

    Update for comments design

--- application/views/templates/comments/comment.php
+++ application/views/templates/comments/comment.php
@@ -1,4 +1,4 @@
-<div class="comment island island--padded clear <?= $comment->parent_comment ? 'answer_wrapper' : 'island--margined' ?>" id="comment_<?= $comment->id ?>">
+<div class="comment island island--padded clear island--margined" id="comment_<?= $comment->id ?>">
 
     <div class="comment__header clearfix">
 
@@ -6,46 +6,53 @@
             <img src="<?= $comment->author->photo ?>">
         </a>
 
+        <?  if ($user->id == $comment->author->id || $user->isAdmin): ?>
+            <a class="comment__actions--button button--delete js-approval-button"
+               href="/p/<?= $page->id ?>/<?= $page->uri ?>/delete-comment/<?= $comment->id ?>">
+               <i class="icon-trash"></i>
+            </a>
+        <? endif ?>
+
         <div class="constrain">
             <a class="comment__author-name" href="/user/<?= $comment->author->id ?>">
                 <?= $comment->author->name ?>
             </a><br>
-            <? /**
-                *
-                * @todo add 'in reply to' cursor
-
-                <? if ($comment->parent_comment): ?>
-                    <span class="comment">
-                        <i class="icon-right-dir"></i>
-                        <?= $comment->parent_comment->author->name ?>
-                    </span>
-                <? endif ?>
-                */
-            ?>
 
             <time class="comment__time">
                 <?= $methods->ftime(strtotime($comment->dt_create)); ?>
             </time>
         </div>
+
     </div>
+
+    <? if ($comment->parent_comment): ?>
+        <div class="comment__replied">
+
+            <div class="comment__replied-source">
+                <?= $comment->parent_comment->text ?>
+            </div>
+
+            <span class="comment__replied-author">
+                <a href="/user/<?= $comment->parent_comment->author->id ?>">
+                    <?= $comment->parent_comment->author->name ?>
+                </a>
+            </span>
+
+            <span class="comment__replied-date">
+                <?= $methods->ftime(strtotime($comment->parent_comment->dt_create)); ?>
+            </span>
+
+            <? /* <span class="comment__replied-page">
+                <?= $comment->parent_comment->text ?>
+            </span> */ ?>
+
+        </div>
+    <? endif ?>
 
     <div class="comment__text">
         <?= $comment->text ?>
     </div>
 
-    <?
-        /**
-        * @todo add 'reply' function
-        */
-    ?>
-
-    <?
-         if ($user->id == $comment->author->id || $user->isAdmin): ?>
-        <a class="comment__actions--button button--delete js-approval-button"
-           href="/p/<?= $page->id ?>/<?= $page->uri ?>/delete-comment/<?= $comment->id ?>">
-           <i class="icon-trash"></i>
-           Удалить
-        </a>
-    <? endif ?>
+    <?= View::factory('templates/comments/new-comment-form', array('page' => $page, 'user' => $user, 'parent_id' => $comment->id, 'root_id' =>  $comment->root_id )); ?>
 
 </div>
