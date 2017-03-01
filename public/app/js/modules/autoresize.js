df40fe9611b29d267d26b2e9ea9b988e2b946ec7
tjarrow <nickita0104@gmail.com>
2017-03-01 00:37:24 -0800

    Correct tabulation

--- public/app/js/modules/autoresize.js
+++ public/app/js/modules/autoresize.js
@@ -1,26 +1,26 @@
 module.exports = (function () {
 
-var textareaClicked = function (event) {
+    var textareaClicked = function (event) {
 
-    var textarea = event.target;
+        var textarea = event.target;
 
-}
+    };
 
-var init = function () {
+    var init = function () {
 
-    var textareas = document.getElementsByClassName('js-autoresizable');
+        var textareas = document.getElementsByClassName('js-autoresizable');
 
-    if (textareas.length) for (var i = 0; i <= textareas.length - 1; i++) {
+        if (textareas.length) for (var i = 0; i <= textareas.length - 1; i++) {
 
-        textareas[i].addEventListener('keydown', textareaClicked, false);
+            textareas[i].addEventListener('keydown', textareaClicked, false);
 
-    }
+        }
 
-};
+    };
 
-var checkScrollHeight = function(textarea) {
+    var checkScrollHeight = function (textarea) {
 
-        if (textarea.scrollHeight > textarea.clientHeight){
+        if (textarea.scrollHeight > textarea.clientHeight) {
 
             textarea.style.height = textarea.scrollHeight + 'px';
 
@@ -30,4 +30,4 @@ var checkScrollHeight = function(textarea) {
 
     };
 
-}
+});
