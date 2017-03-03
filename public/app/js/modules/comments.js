54eddff9215d6a2f0b098547b63fb65ea9598260
Taly Guryn <vitalik7tv@yandex.ru>
2017-03-03 14:55:10 +0300

    Rebuild comments core

--- public/app/js/modules/comments.js
+++ public/app/js/modules/comments.js
@@ -1,33 +1,85 @@
 var comments = (function () {
 
-    var commentsList        = null,
-        sendCommentsFormURL = '',
-        classNameReplyForms = 'comment-form__text';
+    var commentsList = null;
 
     function init() {
 
-        // form     = document.getElementById('comment_form');
-        // textarea = document.getElementById('add_comment_textarea');
         commentsList = document.getElementById('commentsList');
-
-        setListeners();
 
     }
 
-    function setListeners() {
+    function appendForm(elem) {
 
-        var textareas = document.getElementsByClassName(classNameReplyForms);
+        var holder = elem.target,
+            holderParent = holder.parentNode,
+            form  = createForm(holder);
 
-        for (var i = 0; i < textareas.length; i++) {
+        holderParent.removeChild(holder);
+        holderParent.appendChild(form);
 
-            var textarea = textareas[i];
+        returnTextareaByForm(form).focus();
 
-            // ctrl(cmd) + enter
-            textarea.addEventListener('keydown', keydownSubmitHandler_, false);
+    }
 
-            // form.addEventListener('submit', sendFormByAjax_, false);
+    function createForm(holder) {
 
-        }
+        var holderParent = holder.parentNode,
+            textarea     = createTextarea(holder),
+            button       = createButton(),
+            form         = document.createElement('DIV');
+
+        form.dataset.parentid          = holderParent.dataset.parentid;
+        form.dataset.rootid            = holderParent.dataset.rootid;
+        form.dataset.sendcommentaction = holderParent.dataset.sendcommentaction;
+
+        form.appendChild(textarea);
+        form.appendChild(button);
+
+        return form;
+
+    }
+
+    function createTextarea(holder) {
+
+        var textarea = document.createElement('TEXTAREA');
+
+        textarea.classList.add('comment-form__text');
+        textarea.placeholder = holder.innerHTML;
+        textarea.rows = 1;
+        textarea.required = true;
+        textarea.addEventListener('keydown', keydownSubmitHandler_, false);
+
+        return textarea;
+
+    }
+
+    function createButton() {
+
+        var button = document.createElement('DIV');
+
+        button.classList.add('comment-form__button', 'button');
+        button.innerHTML = 'Оставить комментарий';
+        button.addEventListener('click', sendFormByAjax_, false);
+
+        return button;
+
+    }
+
+    function returnTextareaByForm(form) {
+
+        return form.getElementsByTagName('TEXTAREA')[0];
+
+    }
+
+    function createHolder() {
+
+        var holder = document.createElement('DIV');
+
+        holder.classList.add('comment-form__placeholder');
+        holder.addEventListener('click', appendForm, false);
+        holder.innerHTML = 'Ваш комментарий...';
+
+        return holder;
 
     }
 
@@ -49,16 +101,12 @@ var comments = (function () {
 
     function sendFormByAjax_(event) {
 
-        event.preventDefault();
-
-        var rootId   = event.target.dataset.rootid,
-            parentId = event.target.dataset.parentid,
-            text     = event.target.value,
-            formData = new FormData();
-
-        console.log(event.target.dataset);
-
-        sendCommentsFormURL = event.target.dataset.sendcommentaction;
+        var form      = event.target.parentNode,
+            text      = returnTextareaByForm(form).value,
+            formData  = new FormData(),
+            rootId    = form.dataset.rootid,
+            parentId  = form.dataset.parentid,
+            actionURL = form.dataset.sendcommentaction;
 
         formData.append('root_id', rootId);
         formData.append('parent_id', parentId);
@@ -67,7 +115,7 @@ var comments = (function () {
 
         codex.ajax.call({
             type: 'POST',
-            url: sendCommentsFormURL,
+            url: actionURL,
             data: formData,
             beforeSend : function () {},
             success : function (response) {
@@ -76,6 +124,7 @@ var comments = (function () {
 
                 if (response.success) {
 
+                    // если на странице не было ни одного комментария, то удаляем мотиватор
                     if (commentsList.dataset.count == 0) {
 
                         commentsList.innerHTML = '';
@@ -85,11 +134,13 @@ var comments = (function () {
                     commentsList.innerHTML += response.comment;
                     commentsList.dataset.count++;
 
-                    clearForm(event.target);
-
                     window.scrollTo(0, document.body.scrollHeight);
 
-                    setListeners();
+                    var comment = document.getElementById('comment_' + response.commentId);
+
+                    comment.classList.remove('comment--highlited');
+
+                    removeForm(form);
 
                 } else {
 
@@ -103,16 +154,19 @@ var comments = (function () {
 
     }
 
-    function clearForm(textarea) {
+    function removeForm(form) {
 
-        textarea.value = '';
+        var parent = form.parentNode,
+            holder = createHolder();
 
-        textarea.blur();
+        form.remove();
+        parent.appendChild(holder);
 
     }
 
     return {
-        init: init
+        init : init,
+        appendForm : appendForm
     };
 
 }());
