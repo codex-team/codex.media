6f33653d0e9f32cc892ab0cd6c0de4087700c242
N0str <ntpcp@yandex.ru>
2017-03-09 15:01:38 +0300

    Update auth-register layout. Change css (need rebuild).

--- application/views/templates/auth/signup.php
+++ application/views/templates/auth/signup.php
@@ -1,64 +1,61 @@
-
-<style>
-    .sidebar{
-        display: none;
-    }
-</style>
-<div class="page_landing">
+<div class="island island--padded island--centered">
 
     <h1 class="title">Войти на сайт</h1>
-    <div class="desc">Вы можете войти на сайт через аккаунт в социальной сети</div>
+    <p class="desc">Вы можете войти на сайт через аккаунт в социальной сети</p>
 
-    <a class="button iconic vk" href="/auth/vk"><i class="icon-vkontakte"></i>ВКонтакте</a>
-    <a class="button iconic facebook" href="/auth/fb"><i class="icon-facebook"></i>Facebook</a>
-    <a class="button iconic twitter" href="/auth/tw"><i class="icon-twitter"></i>Twitter</a>
+    <a class="button button--vk" href="/auth/vk"><i class="icon-vkontakte"></i>ВКонтакте</a>
+    <a class="button button--facebook" href="/auth/fb"><i class="icon-facebook"></i>Facebook</a>
+    <a class="button button--twitter" href="/auth/tw"><i class="icon-twitter"></i>Twitter</a>
 
-</div>
+    <form class="auth-form" action="/signup" method="post">
 
-<form class="page_simple_form" action="/signup" method="post">
+        <h3>Регистрация</h3>
 
-    <h3>Регистрация</h3>
+        <? $regFields = array(
+            'name' => array(
+                'label' => 'Фамилия, Имя',
+                'value' => isset( $inviteData['name'] ) ? $inviteData['name'] : Arr::get($_POST, 'signup_name', ''),
+            ),
+            'email' => array(
+                'label' => 'Email',
+                'type'  => 'email',
+                'value' => isset( $inviteData['mail'] ) ? $inviteData['mail'] : Arr::get($_POST, 'signup_email'),
+            ),
+            'password' => array(
+                'label' => 'Пароль',
+                'type'  => 'password',
+            ),
+            'password_repeat' => array(
+                'label' => 'Повторите пароль',
+                'type'  => 'password',
+                'id' => 'password_repeat'
+            )
+        ); ?>
 
-    <? $regFields = array(
-        'name' => array(
-            'label' => 'Фамилия, Имя',
-            'value' => isset( $inviteData['name'] ) ? $inviteData['name'] : Arr::get($_POST, 'signup_name', ''),
-        ),
-        'email' => array(
-            'label' => 'Email',
-            'type'  => 'email',
-            'value' => isset( $inviteData['mail'] ) ? $inviteData['mail'] : Arr::get($_POST, 'signup_email'),
-        ),
-        'password' => array(
-            'label' => 'Пароль',
-            'type'  => 'password',
-        ),
-        'password_repeat' => array(
-            'label' => 'Повторите пароль',
-            'type'  => 'password',
-            'id' => 'password_repeat'
-        )
-    ); ?>
-
-    <? if (!empty($signup_error_fields)): ?>
-        <? foreach($signup_error_fields as $fieldName => $errorText ): ?>
-            <div class="auth-error"><?= $errorText ?></div>
-        <? endforeach; ?>
-    <? endif; ?>
-
-    <fieldset>
         <? foreach ($regFields as $fieldName => $field): ?>
-            <input <?= !empty($signup_error_fields[$fieldName]) ? 'class="invalid"' : '' ?> placeholder="<?= Arr::get($field, 'label') ?>" type="<?= Arr::get($field, 'type', 'text') ?>" name="signup_<?= $fieldName?>" id="signup_<?= $fieldName ?>" value="<?= Arr::get($field, 'value') ?>"  <?= Arr::get($field, 'events')?> required />
+            <p>
+                <input placeholder="<?= Arr::get($field, 'label') ?>" type="<?= Arr::get($field, 'type', 'text') ?>" name="signup_<?= $fieldName?>" id="signup_<?= $fieldName ?>" value="<?= Arr::get($field, 'value') ?>" />
+            </p>
         <? endforeach ?>
-    </fieldset>
 
-    <?= Form::hidden('csrf', Security::token()); ?>
-    <?= Form::hidden('action', 'signup' ); ?>
+        <?= Form::hidden('csrf', Security::token()); ?>
+        <?= Form::hidden('action', 'signup' ); ?>
 
-    <button class="button main">Зарегистрироваться</button>
-</form>
+        <? if (!empty($signup_error_fields)): ?>
+            <? foreach($signup_error_fields as $fieldName => $errorText ): ?>
+                <div class="auth-form__error"><?= $errorText ?></div>
+            <? endforeach; ?>
+        <? endif; ?>
 
-<div class="page_footer_links">
-    <a href="/recover">Восстановить пароль</a>
-    <a href="/auth">Войти на сайт</a>
+        <button class="button master">Зарегистрироваться</button>
+
+    </form>
+
+    <div class="auth-form__footer">
+        <a href="/recover">Восстановить пароль</a>
+        <a href="/auth">Вход</a>
+    </div>
+
 </div>
+
+
