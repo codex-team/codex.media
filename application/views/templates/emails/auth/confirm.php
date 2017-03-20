db269a715184c03876d7a0a8b12b406a03ae4d0d
Taly Guryn <vitalik7tv@yandex.ru>
2017-03-20 17:05:11 +0300

    Add http(s)

--- application/views/templates/emails/auth/confirm.php
+++ application/views/templates/emails/auth/confirm.php
@@ -1,10 +1,10 @@
 <h2>Здравствуйте, <?= $user->name; ?>!</h2>
 <div>
     <p>
-        Вы зарегистрировались на <a href="<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы подтвердить адрес электронной почты, перейдите по следующей ссылке:
+        Вы зарегистрировались на <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы подтвердить адрес электронной почты, перейдите по следующей ссылке:
     </p>
     <p>
-        <a href="<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?></a>
+        <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?></a>
     </p>
     <p>
         Если вы получили это письмо по ошибке, можете проигнорировать его.
--- application/views/templates/emails/auth/reset.php
+++ application/views/templates/emails/auth/reset.php
@@ -1,10 +1,10 @@
 <h2>Здравствуйте, <?= $user->name; ?>!</h2>
 <div>
     <p>
-        Вы запросили сброс пароля на <a href="<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы установить новый пароль, перейдите по следующей ссылке:
+        Вы запросили сброс пароля на <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы установить новый пароль, перейдите по следующей ссылке:
     </p>
     <p>
-        <a href="<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?></a>
+        <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?></a>
     </p>
     <p>
         Если вы не запрашивали смену пароля, то проигнорируйте это письмо.
