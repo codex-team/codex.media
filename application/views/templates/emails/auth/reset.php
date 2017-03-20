eb0813bdd04faa7e17f21870c7a14949e392c1cc
Taly Guryn <vitalik7tv@yandex.ru>
2017-03-20 16:56:06 +0300

    New emails

--- application/views/templates/emails/auth/reset.php
+++ application/views/templates/emails/auth/reset.php
@@ -1,8 +1,12 @@
-<h2>Здравствуйте, <?= $user->name; ?></h2>
+<h2>Здравствуйте, <?= $user->name; ?>!</h2>
 <div>
-    Вы запросили сброс пароля на <a href="<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы сменить пароль,
-    перейдите по <a href="<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?>" style="color: blue;">ссылке</a>.
-</div>
-<div>
-    Если вы не запрашивали смену пароля, можете смело проигнорировать это письмо.
+    <p>
+        Вы запросили сброс пароля на <a href="<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы установить новый пароль, перейдите по следующей ссылке:
+    </p>
+    <p>
+        <a href="<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?>" style="color: blue;"><?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?></a>
+    </p>
+    <p>
+        Если вы не запрашивали смену пароля, то проигнорируйте это письмо.
+    </p>
 </div>
