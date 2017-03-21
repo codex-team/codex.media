1cf0d15d5d77da981aefa40afab4b2fc6d718929
Taly Guryn <vitalik7tv@yandex.ru>
2017-03-21 15:42:38 +0300

    Update email

--- application/views/templates/emails/auth/confirm.php
+++ application/views/templates/emails/auth/confirm.php
@@ -1,10 +1,14 @@
+<?
+    $confirmProfile = "http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?>";
+?>
+
 <h2>Здравствуйте, <?= $user->name; ?>!</h2>
 <div>
     <p>
-        Вы зарегистрировались на <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?></a>. Чтобы подтвердить адрес электронной почты, перейдите по следующей ссылке:
+        Вы зарегистрировались на <?= $_SERVER['HTTP_HOST']; ?>. Чтобы подтвердить адрес электронной почты, перейдите по следующей ссылке:
     </p>
     <p>
-        <a href="http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?>" style="color: blue;">http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?></a>
+        <a href="<?= $confirmProfile ?>" style="color: blue;"><?= $confirmProfile ?></a>
     </p>
     <p>
         Если вы получили это письмо по ошибке, можете проигнорировать его.
