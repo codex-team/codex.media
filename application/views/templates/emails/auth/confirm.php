4206ef62663f55d136099e0e84b5887546f30270
Taly Guryn <vitalik7tv@yandex.ru>
2017-03-21 15:55:03 +0300

    update

--- application/views/templates/emails/auth/confirm.php
+++ application/views/templates/emails/auth/confirm.php
@@ -1,10 +1,14 @@
 <?
-    $confirmProfile = "http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/confirm/<?= $hash; ?>";
+    $protocol = "http" . (Arr::get($_SERVER, 'HTTPS') ? 's' : '') . "://";
+    $host     = $_SERVER['HTTP_HOST'];
+    $uri      = "/confirm/" . $hash;
+
+    $link = $protocol . $host . $uri;
 ?>
 Здравствуйте, <?= $user->name; ?>!
 
 Вы зарегистрировались на <?= $_SERVER['HTTP_HOST']; ?>. Чтобы подтвердить адрес электронной почты, перейдите по следующей ссылке:
 
-<?= $confirmProfile ?>
+<?= $link ?>
 
 Если вы получили это письмо по ошибке, можете проигнорировать его.
--- application/views/templates/emails/auth/reset.php
+++ application/views/templates/emails/auth/reset.php
@@ -1,10 +1,14 @@
 <?
-    $resetPasswordLink = "http<?= Arr::get($_SERVER, 'HTTPS') ? 's' : '' ?>://<?= $_SERVER['HTTP_HOST']; ?>/reset/<?= $hash; ?>";
+    $protocol = "http" . (Arr::get($_SERVER, 'HTTPS') ? 's' : '') . "://";
+    $host     = $_SERVER['HTTP_HOST'];
+    $uri      = "/reset/" . $hash;
+
+    $link = $protocol . $host . $uri;
 ?>
 Здравствуйте, <?= $user->name; ?>!
 
 Вы запросили сброс пароля на <?= $_SERVER['HTTP_HOST']; ?>. Чтобы установить новый пароль, перейдите по следующей ссылке:
 
-<?= $resetPasswordLink ?>
+<?= $link ?>
 
 Если вы получили это письмо по ошибке, можете проигнорировать его.
