6ff3bf77dbaa5b152736ee0c892e309e4c32b84e
George Berezhnoy <gohabereg@gmail.com>
2017-04-02 18:46:26 +0300

    Upd

--- application/views/templates/pages/writing.php
+++ application/views/templates/pages/writing.php
@@ -1,29 +1,27 @@
-<div class="writing-fullscreen">
-    <div class="island island--top-rounded writing-navigation__holder">
+<div class="island island--top-rounded writing-navigation__holder">
 
-        <div class="writing-navigation">
+    <div class="writing-navigation">
 
-            <a href="/user/<?= $user->id; ?>" rel="nofollow">
-                <img class="writing-navigation__back-icon" src="<?= $user->photo ?>">
-            </a>
-            <a href="/" rel="nofollow">
-                Главная
-            </a>
-            <? include(DOCROOT . "public/app/svg/arrow-right.svg") ?>
-            <?
-                $action = isset($page->id) && $page->id ? 'Редактирование' : 'Создание';
-                $object = $page->isNewsPage() ? 'новости' : 'материала';
+        <a href="/user/<?= $user->id; ?>" rel="nofollow">
+            <img class="writing-navigation__back-icon" src="<?= $user->photo ?>">
+        </a>
+        <a href="/" rel="nofollow">
+            Главная
+        </a>
+        <? include(DOCROOT . "public/app/svg/arrow-right.svg") ?>
+        <?
+            $action = isset($page->id) && $page->id ? 'Редактирование' : 'Создание';
+            $object = $page->isNewsPage() ? 'новости' : 'материала';
 
-                echo $action . ' ' . $object;
-            ?>
-
-        </div>
+            echo $action . ' ' . $object;
+        ?>
 
     </div>
 
-    <div class="writing--fullscreen">
-        <?= View::factory('templates/pages/form', array(
-            'page' => $page
-        )); ?>
-    </div>
+</div>
+
+<div class="writing--fullscreen">
+    <?= View::factory('templates/pages/form', array(
+        'page' => $page
+    )); ?>
 </div>
