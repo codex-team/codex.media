0cee412f1a64679d647aa31afd6f1b2a5c051d46
George Berezhnoy <gohabereg@gmail.com>
2017-04-19 21:22:21 +0300

    Pinned news

--- application/views/templates/pages/list.php
+++ application/views/templates/pages/list.php
@@ -16,7 +16,11 @@
                 <? endif ?>
                 <time class="post-list-item__date">
                     <a href="<?= $page->url ?>">
-                        <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
+                        <? if ($page->isPinned): ?>
+                            Запись закреплена
+                        <? else: ?>
+                            <?= date_format(date_create($page->date), 'd F Y, G:i') ?>
+                        <? endif; ?>
                     </a>
                 </time>
                 <a class="post-list-item__author" href="/user/<?= $page->author->id ?>">
@@ -64,24 +68,30 @@
 
                 /** Island settings menu */
                 codex.islandSettings.init({
-                    selector : '.js-page-settings',
-                    items : [
+                    selector: '.js-page-settings',
+                    items: [
                         {
-                            title : 'Редактировать',
-                            handler : codex.pages.openWriting
+                            title: 'Редактировать',
+                            handler: codex.pages.openWriting
                         },
                         {
-                            title : 'Удалить',
-                            handler : codex.pages.remove
-                        }
+                            title: 'Удалить',
+                            handler: codex.pages.remove
+                        },
                         <? if ($user->isAdmin): ?>
-                            ,
-                            {
-                                title: 'Установить обложку',
-                                handler : codex.pages.cover.toggleButton
-                            }
+                        {
+                            title: 'Установить обложку',
+                            handler: codex.pages.cover.toggleButton
+                        },
+                        <? if ($active_tab == Model_Feed_Pages::TYPE_NEWS): ?>
+                        {
+                            title: 'Закрепить',
+                            handler: codex.pages.pin
+                        }
                         <? endif; ?>
-                    ]});
+                        <? endif; ?>
+                    ]
+                })
             });
         </script>
     <? endif ?>
