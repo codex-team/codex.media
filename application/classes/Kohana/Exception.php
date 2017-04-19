8e4d8c99d45bdbe7eb1ffe011e8a1eba01c286b6
George Berezhnoy <gohabereg@gmail.com>
2017-04-19 18:45:02 +0300

    smth

---
+++ application/classes/Kohana/Exception.php
@@ -0,0 +1,40 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kohana_Exception extends Kohana_Kohana_Exception {
+
+    public static function _handler($e)
+    {
+        switch (Kohana::$environment) {
+            case Kohana::PRODUCTION:
+                self::$error_view = 'templates/errors/500';
+                self::formatErrorForTelegrams($e);
+            break;
+
+            case Kohana::TESTING:
+            case Kohana::STAGING:
+                self::$error_view = 'templates/errors/500';
+            break;
+        }
+
+        return parent::_handler($e);
+    }
+
+    /**
+     * Compose error trace for Telegram
+     * @param Exception $e - kohana exception object
+     */
+    private static function formatErrorForTelegrams( $e )
+    {
+        $protocol = HTTP::$protocol == 'HTTP' ? 'http://' : 'https://';
+        if (!empty(Request::current())){
+            $path = $protocol . Arr::get($_SERVER, 'SERVER_NAME') . Request::current()->url();
+        } else {
+            $path = '';
+        }
+        $telegramMsg = '⚠️ ' . $e->getMessage() . '';
+        $telegramMsg .= PHP_EOL .  PHP_EOL . $e->getFile() . ': ' . $e->getLine() .  PHP_EOL . PHP_EOL;
+        $telegramMsg .= $path;
+        Model_Methods::sendBotNotification($telegramMsg);
+
+    }
+}
