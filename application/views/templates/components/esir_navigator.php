33290b8c63958cedd443415149fe27cf22723e4e
Peter Savchenko <specc.dev@gmail.com>
2017-04-25 14:23:56 +0300

    ESIR navigator 👋

---
+++ application/views/templates/components/esir_navigator.php
@@ -0,0 +1,9 @@
+<div class="esir" onclick="document.body.classList.add('esir-showed'); var esir = document.getElementById('widgetEsirGovSpbRu'); esir && esir.click();">
+    <div class="esir__included">
+        Сайт включен в каталог ЕСИР
+    </div>
+    <div class="esir__navigator">
+        Навигатор по гос. сайтам Санкт-Петербурга
+    </div>
+    <div class="esir__search">Поиск по сайту</div>
+</div>
