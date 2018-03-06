cbef60988145c18c188426045badb954820f485b
Taly <vitalik7tv@yandex.ru>
2018-03-06 15:48:40 +0300

    show article id on error

--- application/classes/Model/Page.php
+++ application/classes/Model/Page.php
@@ -238,7 +238,7 @@ class Model_Page extends Model
 
             return $CodexEditor->getBlocks($escapeHTML);
         } catch (Exception $e) {
-            throw new Kohana_Exception("CodexEditor: " . $e->getMessage());
+            throw new Kohana_Exception("CodexEditor (article:" . $this->id . "): " . $e->getMessage());
         }
     }
 
