1b42723e972ad7b477a41dbc10b9c97422f8eab4
Taly <vitalik7tv@yandex.ru>
2018-05-22 19:12:55 +0300

    move everything to www dir

---
+++ www/migrations/2017-04-13-codex-org.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 12, 2017 at 10:24 PM
+-- Server version: 5.7.16
+-- PHP Version: 5.6.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `codex-org`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `text` text NOT NULL,
+  `page_id` int(10) UNSIGNED NOT NULL,
+  `root_id` int(10) UNSIGNED NOT NULL,
+  `parent_id` int(10) UNSIGNED NOT NULL,
+  `dt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_removed` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `target` int(10) UNSIGNED DEFAULT NULL,
+  `filename` varchar(100) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `author` int(10) UNSIGNED NOT NULL,
+  `size` float UNSIGNED NOT NULL,
+  `extension` varchar(5) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_removed` tinyint(1) NOT NULL DEFAULT '0',
+  `status` tinyint(3) DEFAULT '0',
+  `type` tinyint(13) NOT NULL COMMENT 'Тип файла из контроллера Transport',
+  `file_hash` binary(16) DEFAULT NULL,
+  `mime` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 - show, 1 - hide , 2 - removed',
+  `id_parent` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `title` varchar(255) NOT NULL,
+  `content` text,
+  `cover` varchar(255) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `author` int(10) UNSIGNED NOT NULL,
+  `rich_view` tinyint(1) DEFAULT '0',
+  `dt_pin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `name` varchar(50) NOT NULL,
+  `value` varchar(150) DEFAULT NULL,
+  `label` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`name`, `value`, `label`) VALUES
+('address', '193312 Санкт-Петербург, Товарищеский пр., д. 10, корп. 2', 'site_info'),
+('city', 'Санкт-Петербург', 'site_info'),
+('coordinates', '59.919041, 30.4875325', 'site_info'),
+('description', 'Приветствуем на официальном сайте школы №332 Санкт-Петербурга', 'site_info'),
+('email', 'spb_school332@mail.ru', 'site_info'),
+('fax', '580-82-49', 'site_info'),
+('full_name', 'ГБОУ СОШ №332 Невского района', 'site_info'),
+('phone', '580-89-08; 584-54-98', 'site_info'),
+('title', 'Школа 332', 'site_info');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `bio` text,
+  `photo` varchar(255) DEFAULT NULL,
+  `photo_medium` varchar(255) DEFAULT NULL,
+  `photo_big` varchar(255) DEFAULT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
+  `twitter_name` varchar(255) DEFAULT NULL,
+  `twitter_username` varchar(255) DEFAULT NULL,
+  `vk` varchar(255) DEFAULT NULL,
+  `vk_name` varchar(255) DEFAULT NULL,
+  `vk_uri` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `facebook_name` varchar(255) DEFAULT NULL,
+  `facebook_username` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 — normal, 1 — banned',
+  `role` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 — registred, 2 — teacher, 3 — admin',
+  `dt_reg` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_removed` tinyint(1) NOT NULL DEFAULT '0',
+  `isConfirmed` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_sessions`
+--
+
+CREATE TABLE `users_sessions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `uid` int(10) UNSIGNED NOT NULL,
+  `cookie` varchar(100) NOT NULL,
+  `dt_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dt_access` timestamp NULL DEFAULT NULL,
+  `dt_close` timestamp NULL DEFAULT NULL,
+  `useragent` text NOT NULL,
+  `social_provider` tinyint(3) UNSIGNED DEFAULT NULL COMMENT '1 — vk, 2 — fb , 3 — tw',
+  `ip` bigint(11) NOT NULL DEFAULT '0',
+  `autologin` smallint(4) DEFAULT NULL COMMENT 'autologin type'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`id_parent`),
+  ADD KEY `id_parent` (`id_parent`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_sessions`
+--
+ALTER TABLE `users_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users_sessions`
+--
+ALTER TABLE `users_sessions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
