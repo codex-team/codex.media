9d2089a9af0bff318bce7eda65de4a1e0f19fb9e
Polina Shneider <beatrixlinkoln@gmail.com>
2018-06-18 15:24:05 +0300

    Delete build folder

--- www/projects/cultofart/views/templates/components/about.php
+++ www/projects/cultofart/views/templates/components/about.php
@@ -1,13 +1,13 @@
 <div class="island about js-emoji-included">
-    <h3 class="about__title">
+    <h3 class="island__title">
         <a href="">
             Our mission
         </a>
     </h3>
-     <p class="about__body">
+     <p class="island__caption">
          Вообще "миссия" это текст 1000 знаков. Он важен для понимания нашей деятельности и наших ценностей. Поэтому он нужен в первую очередь...
      </p>
-     <a href="" class="about__more">
+     <a href="" class="island__link">
          Read more about Culture of Art »
      </a>
 </div>
