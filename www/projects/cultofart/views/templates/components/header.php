962cba4690ec35746e9c97266d4f1bf7496a1b78
Polina Shneider <beatrixlinkoln@gmail.com>
2018-06-19 00:35:26 +0300

    Site header

---
+++ www/projects/cultofart/views/templates/components/header.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <div class="header__content center-col">
+        <a href="" class="header__title">
+            Culture of Art
+        </a>
+        <span class="header__address">
+            St. Petersburg, Birzhevaya 12
+        </span>
+        <div class="header__lang">
+            <span class="header__lang-item">ru</span>
+            <span class="header__lang-item">en</span>
+        </div>
+        <a href="" class="header__button">Support the Project</a>
+        <span class="mobile-menu-toggler header__mobile-menu-toggler" id="js-mobile-menu-toggler">
+            <? include(DOCROOT . "public/app/svg/menu.svg") ?>
+        </span>
+    </div>
+</header>
