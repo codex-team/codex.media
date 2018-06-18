0e1da9082829ec0a272a7cdc772cc4393052fd35
Polina Shneider <beatrixlinkoln@gmail.com>
2018-06-18 15:14:27 +0300

    Edit template

--- www/projects/cultofart/views/templates/components/about.php
+++ www/projects/cultofart/views/templates/components/about.php
@@ -1,13 +1,13 @@
 <div class="island about js-emoji-included">
-
     <h3 class="about__title">
-        <a href="">Our mission</a>
+        <a href="">
+            Our mission
+        </a>
     </h3>
-
      <p class="about__body">
          Вообще "миссия" это текст 1000 знаков. Он важен для понимания нашей деятельности и наших ценностей. Поэтому он нужен в первую очередь...
      </p>
-
-     <a href="" class="about__more">Read more about Culture of Art »</a>
-
+     <a href="" class="about__more">
+         Read more about Culture of Art »
+     </a>
 </div>
