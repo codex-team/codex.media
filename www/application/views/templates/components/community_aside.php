2f9e0ae5a14542d0f8e24639c8cbed4fd2784a43
Polina Shneider <beatrixlinkoln@gmail.com>
2018-06-23 18:50:53 +0300

    Update logo placeholder

--- www/application/views/templates/components/community_aside.php
+++ www/application/views/templates/components/community_aside.php
@@ -1,10 +1,12 @@
 <aside class="island main-aside">
     <div class="community-aside">
-        <? if (!empty($page->cover)): ?>
-            <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
+        <a href="/p/<?= $page->id ?>/<?= $page->uri ?>">
+            <? if (!empty($page->cover)): ?>
                 <img class="community-aside__logo" src="/upload/pages/covers/b_<?= $page->cover ?>">
-            </a>
-        <? endif; ?>
+            <? else: ?>
+                <img class="community-aside__logo community-aside__logo--default" src="/public/app/svg/community-placeholder.svg">
+            <? endif; ?>
+        </a>
         <a href="/p/<?= $page->id ?>/<?= $page->uri ?>" class="community-aside__title">
             <?= $page->title ?>
         </a>
