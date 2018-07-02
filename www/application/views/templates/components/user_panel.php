df54c99e1f4e9f58b374272988c1d7c587f59382
Peter Savchenko <specc.dev@gmail.com>
2018-07-02 17:00:36 +0300

    Adoptation for tablet devices

--- www/application/views/templates/components/user_panel.php
+++ www/application/views/templates/components/user_panel.php
@@ -1,11 +1,19 @@
-<div class="user-panel clearfix">
-    <a class="fl_r user-panel__logout" href="/logout" data-title="Выйти">
-        <i class="icon-logout"></i>
-    </a>
-    <a href="/user/<?= $user->id ?>">
-        <img src="<?= $user->photo ?>" name="js-img-updatable" />
-        <span class="user-panel__name">
+<? if ($user->id): ?>
+    <div class="island">
+        <div class="user-panel clearfix">
+            <a class="fl_r user-panel__logout" href="/logout" data-title="Выйти">
+                <i class="icon-logout"></i>
+            </a>
+            <a href="/user/<?= $user->id ?>">
+                <img src="<?= $user->photo ?>" name="js-img-updatable" />
+                <span class="user-panel__name">
             <?= $user->shortName ?>
         </span>
-    </a>
-</div>
+            </a>
+        </div>
+    </div>
+<? elseif (empty($showAuth) || $showAuth !== false ): ?>
+    <div class="island island--padded">
+        <a class="button master" href="/auth">Войти на сайт</a>
+    </div>
+<? endif ?>
--- www/projects/cultofart/views/templates/components/right_col.php
+++ www/projects/cultofart/views/templates/components/right_col.php
@@ -1,11 +1,3 @@
 <div class="grid-col grid-col--right">
-    <? if ($user->id): ?>
-        <div class="island">
-            <?= View::factory('templates/components/user_panel')->render(); ?>
-        </div>
-    <? else: ?>
-        <div class="island island--padded">
-            <a class="button master" href="/auth">Войти на сайт</a>
-        </div>
-    <? endif ?>
+    <?= View::factory('templates/components/user_panel')->render(); ?>
 </div>
