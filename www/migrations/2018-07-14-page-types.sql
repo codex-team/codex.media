ab0d9854769a4287270a1d5bc9a4520e2289ed7f
Polina Shneider <beatrixlinkoln@gmail.com>
2018-07-20 16:12:11 +0300

    Update migrations

--- www/migrations/2018-07-14-page-types.sql
+++ www/migrations/2018-07-14-page-types.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `pages` ADD `type` TINYINT(1) NOT NULL DEFAULT '1' AFTER `is_event` COMMENT '1 - page, 2 - blog, 3 - news, 4 - community, 5 - event';
+ALTER TABLE `pages` ADD `type` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 - page , 2 - blog , 3 - news , 4 - community , 5 - event' AFTER `is_event`;
 UPDATE `pages` SET `type` = '5' WHERE `is_event` = '1';
 UPDATE `pages` SET `type` = '4' WHERE `is_community` = '1';
 ALTER TABLE `pages` DROP `is_event`;
