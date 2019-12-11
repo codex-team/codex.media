c56925338b33a2e5714ca23c7348aa4acf31993f
Polina Shneider <beatrixlinkoln@gmail.com>
2019-12-11 23:05:04 +0300

    Update esir_navigator.php

--- www/application/views/templates/components/esir_navigator.php
+++ www/application/views/templates/components/esir_navigator.php
@@ -5,5 +5,6 @@
     <div class="schools-navigator__navigator">
         Навигатор по гос. сайтам Санкт-Петербурга
     </div>
+    <div class="schools-navigator__search">Поиск по сайту</div>
 </div>
 <? include(DOCROOT . 'public/app/svg/clip.svg') ?>
