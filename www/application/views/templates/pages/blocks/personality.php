1518808ca5c1eaf271866bde31c18587096cc903
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-10 20:00:56 +0300

    Add fallback

--- www/application/views/templates/pages/blocks/personality.php
+++ www/application/views/templates/pages/blocks/personality.php
@@ -1,3 +1,11 @@
+<?
+
+if (!isset($block['photo'])) {
+    $block['photo'] = '';
+}
+
+?>
+
 <div class="article__person">
     <div class="article__person-photo" style="<?= $block['photo'] ? 'background-image: url(' . $block['photo'] . '); background-size: contain;' : '' ?>"></div>
     <div class="article__person-name"><?= $block['name'] ?></div>
