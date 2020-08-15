0f774b7603c5f8263fa31ca7af00aef09073cc43
Taly <vitalik7tv@yandex.ru>
2020-08-15 08:01:52 +0300

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,13 @@
 
 echo "Initializing codex.media..."
 
+echo -e "Dropping all changes... \c"
+  rm .env  2>/dev/null
+  rm www/.env
+  rm -rf ./www/application/{logs,cache}
+  rm ./www/application/config/database.php
+echo "OK"
+
 echo -e "Creating .env file for docker... \c"
   rm .env
   cp sample.env .env
@@ -22,7 +29,6 @@ echo -e "Creating .env file for project... \c"
 echo "OK"
 
 echo -e "Creating logs and cache dirs... \c"
-  rm -rf ./www/application/{logs,cache}
   mkdir ./www/application/{logs,cache}
   chmod 777 ./www/application/{logs,cache}
 echo "OK"
@@ -30,4 +36,17 @@ echo "OK"
 echo -e "Setting up rights for upload directory... \c"
   chmod -R 777 ./www/upload
 echo "OK"
+
+echo -e "Copy database config file... \c"
+  cp ./www/application/config/database.sample.php ./www/application/config/database.php
+
+  if [[ $(uname) =~ Darwin|BSD ]]
+  then
+    sed -i "" "s/root_password/$GENERATED_MYSQL_PASSWORD/g" ./www/application/config/database.php
+  else
+    sed -i "s/root_password/$GENERATED_MYSQL_PASSWORD/g" ./www/application/config/database.php
+  fi
+echo "OK"
+
+cat ./www/application/config/database.php
 
