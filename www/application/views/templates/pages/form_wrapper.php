4bfe1182c289bbf0173d6e836eb1ea2a68d583d7
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-15 13:21:55 +0300

    Editor UI fixes
    Closes #494

--- www/application/views/templates/pages/form_wrapper.php
+++ www/application/views/templates/pages/form_wrapper.php
@@ -10,7 +10,7 @@
         <span class="writing-wrapper__placeholder-name"><?= $user->name ?></span>
     </div>
 
-    <span class="hide" id="writingForm">
+    <span class="hide writing-form" id="writingForm">
         <?= View::factory('templates/pages/form', [
             'hideEditorToolbar' => true,
             'community_parent_id' => !empty($community_parent_id) ? $community_parent_id : 0
